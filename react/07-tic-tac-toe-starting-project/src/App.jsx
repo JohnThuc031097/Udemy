14402544bc4ed176d659898be88c6b88bb6f0a1b
ThucNobita <johnthuc1997@gmail.com>
2024-01-19 17:29:49 +0700

    init

--- react/07-tic-tac-toe-starting-project/src/App.jsx
+++ react/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,14 +1,53 @@
+import { useState } from "react";
+import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 
 function App() {
+    const [activePlayer, setActivePlayer] = useState("X");
+    const [gameTurns, setGameTurns] = useState([]);
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setActivePlayer((curActivePlayer) =>
+            curActivePlayer === "X" ? "O" : "X"
+        );
+        setGameTurns((prevTurns) => {
+            let currentPlayer = "X";
+
+            if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+                currentPlayer = "O";
+            }
+
+            const updateTurns = [
+                {
+                    square: { row: rowIndex, col: colIndex },
+                    player: currentPlayer,
+                },
+                ...prevTurns,
+            ];
+
+            return updateTurns;
+        });
+    }
+
     return (
         <main>
             <div id="game-container">
-                <ol id="players">
-                    <Player initialName="Player 1" symbol="X" />
-                    <Player initialName="Player 2" symbol="O" />
+                <ol id="players" className="highlight-player">
+                    <Player
+                        initialName="Player 1"
+                        symbol="X"
+                        isActive={activePlayer === "X"}
+                    />
+                    <Player
+                        initialName="Player 2"
+                        symbol="O"
+                        isActive={activePlayer === "O"}
+                    />
                 </ol>
-                GAME BOARD
+                <GameBoard
+                    onSelectSquare={handleSelectSquare}
+                    turns={gameTurns}
+                />
             </div>
             LOG
         </main>
