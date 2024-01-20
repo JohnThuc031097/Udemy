067ecc25fd78d6647d2e53cd98204e930ada6ff0
ThucNobita <johnthuc1997@gmail.com>
2024-01-21 03:32:24 +0700

    init

--- react/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ react/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,37 +1,22 @@
-import { useState } from "react";
+// const initialGameBoard = [
+//     [null, null, null],
+//     [null, null, null],
+//     [null, null, null],
+// ];
 
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-];
+export default function GameBoard({ onSelectSquare, board }) {
+    // let gameBoard = initialGameBoard;
 
-export default function GameBoard({ onSelectSquare, turns }) {
-    let gameBoard = initialGameBoard;
+    // for (const turn of turns) {
+    //     const { square, player } = turn;
+    //     const { row, col } = square;
 
-    for (const turn of turns) {
-        const { square, player } = turn;
-        const { row, col } = square;
-
-        gameBoard[row][col] = player;
-    }
-
-    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-    // function handleSelectSquare(rowIndex, colIndex) {
-    //     setGameBoard((prevGameBoard) => {
-    //         const updatedBoard = [
-    //             ...prevGameBoard.map((innerArray) => [...innerArray]),
-    //         ];
-    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-    //         return updatedBoard;
-    //     });
-    //     onSelectSquare();
+    //     gameBoard[row][col] = player;
     // }
 
     return (
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => (
+            {board.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
@@ -40,6 +25,7 @@ export default function GameBoard({ onSelectSquare, turns }) {
                                     onClick={() =>
                                         onSelectSquare(rowIndex, colIndex)
                                     }
+                                    disabled={playerSymbol !== null}
                                 >
                                     {playerSymbol}
                                 </button>
