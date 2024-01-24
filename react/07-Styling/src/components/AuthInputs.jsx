be13b24c72f7fe5033a25293351f251133b677e9
ThucNobita <johnthuc1997@gmail.com>
2024-01-24 22:45:35 +0700

    init

--- react/07-Styling/src/components/AuthInputs.jsx
+++ react/07-Styling/src/components/AuthInputs.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
-import { styled } from "styled-components";
+// import { styled } from "styled-components";
 
 import Button from "./Button";
-import CustomInput from "./Input";
+import Input from "./Input";
 
-const ControlContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-    margin-bottom: 1.5rem;
-`;
+// const ControlContainer = styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     gap: 0.5rem;
+//     margin-bottom: 1.5rem;
+// `;
 
 export default function AuthInputs() {
     const [enteredEmail, setEnteredEmail] = useState("");
@@ -32,9 +32,12 @@ export default function AuthInputs() {
     const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
     return (
-        <div id="auth-inputs">
-            <ControlContainer>
-                <CustomInput
+        <div
+            id="auth-inputs"
+            className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800"
+        >
+            <div className="flex flex-col gap-2 mb-6">
+                <Input
                     label={"Email"}
                     invalid={emailNotValid}
                     type="email"
@@ -42,7 +45,7 @@ export default function AuthInputs() {
                         handleInputChange("email", event.target.value)
                     }
                 />
-                <CustomInput
+                <Input
                     label={"Password"}
                     invalid={passwordNotValid}
                     type="password"
@@ -50,9 +53,9 @@ export default function AuthInputs() {
                         handleInputChange("password", event.target.value)
                     }
                 />
-            </ControlContainer>
+            </div>
             <div className="actions">
-                <button type="button" className="text-button">
+                <button type="button" className="border-none text-stone-300">
                     Create a new account
                 </button>
                 <Button onClick={handleLogin}>Sign In</Button>
--- react/07-Styling/src/components/Header.jsx
+++ react/07-Styling/src/components/Header.jsx
@@ -46,14 +46,18 @@ import logo from "../assets/logo.png";
 
 export default function Header() {
     return (
-        <header className="flex flex-col items-center justify-center mt-8 mb-8">
+        <header className="flex flex-col items-center justify-center mt-8 mb-8 md:mb-16">
             <img
                 className="object-contain mb-8 w-44 h-44"
                 src={logo}
                 alt="A canvas"
             />
-            <h1>ReactArt</h1>
-            <p>A community of artists and art-lovers.</p>
+            <h1 className="text-2xl md:text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title mr-0">
+                ReactArt
+            </h1>
+            <p className="text-center text-stone-500 mr-0">
+                A community of artists and art-lovers.
+            </p>
         </header>
     );
 }
